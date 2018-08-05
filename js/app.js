b0616fe8a62745424ace6ac08af649267b823191
Kody Broussard <kzb801@gmail.com>
2018-08-05 18:26:48 -0500

    Added Computer Player
    Added computer player using minimax recursion method

--- js/app.js
+++ js/app.js
@@ -2,8 +2,9 @@
 //Kody Broussard
 //7/9/2018
 //Module Pattern used with vanilla JS
+//Used the following websites to aid in creating the Player VS Computer portion of the code: https://medium.freecodecamp.org/how-to-make-your-tic-tac-toe-game-unbeatable-by-using-the-minimax-algorithm-9d690bad4b37 & https://www.neverstopbuilding.com/blog/2013/12/13/tic-tac-toe-understanding-the-minimax-algorithm13/
 
-//const  ticTacToeModule = (function() {
+const  ticTacToeModule = (function() {
     //Global Variables of ticTacToeModule
     const boardGame = document.getElementById('board');
     const startGame = document.getElementById('start');
@@ -18,7 +19,10 @@
         constructor() {
             this.boxes = document.querySelector('.boxes');
             this.allBoxes = document.querySelectorAll('.box');
-            this.allBoxes2 = document.querySelector('.box');
+            this.allBoxesArray = [...document.querySelectorAll('.box')];
+            this.compBoard = [0, 1, 2, 3, 4, 5, 6, 7, 8,];
+            this.boardMoves = [];
+            this.emptyCells = [];
             this.boardLayout();
         }
         boardLayout() {
@@ -26,18 +30,23 @@
                 if (0 <= j && j < 3) {
                     box2.setAttribute('data-row', 0);
                     box2.setAttribute('data-col', j);
+                    box2.setAttribute('cell-num', j)
                 } else if (3 <= j && j < 6) {
                     box2.setAttribute('data-row', 1);
-                    box2.setAttribute('data-col', j-3);           
+                    box2.setAttribute('data-col', j-3); 
+                    box2.setAttribute('cell-num', j)
                 } else if (6 <= j && j < 9){
                     box2.setAttribute('data-row', 2);
                     box2.setAttribute('data-col', j-6);
+                    box2.setAttribute('cell-num', j)
                 }
             });            
         }
     }
     const newBoard = new Board();
+    const fullBoard = newBoard.allBoxesArray.map(full => full.getAttribute('data-row') + full.getAttribute('data-col'));
     
+
     class Player {
         constructor(playerNumber, numWord) {
             this.playerNumber = playerNumber;
@@ -62,21 +71,19 @@
         playerOneActive();
     }());
 
-    //Start Button - Start Screen disappears and Board Appears
-    
-    //Need to add ClassSyntax for the player
-    //Need to store box coordinates into an arry to test winner
     
     //Game Play
     function playerOneActive() {
         playerOne.activePlayer.classList.add('active');
         playerTwo.activePlayer.classList.remove('active');
+        computer.activePlayer.classList.remove('active');
     }   
     
     function playerTwoActive() {
         playerOne.activePlayer.classList.remove('active');
         playerTwo.activePlayer.classList.add('active');
-    }   
+    }
+       
     
     function boxDisplay(mouseoverTarget) {
         if (mouseoverTarget.classList.contains('alreadySelected') === false){
@@ -93,22 +100,107 @@
     }
     
     function boxDisplayNone(mouseoutTarget) {
-        
         if (mouseoutTarget.classList.contains('alreadySelected') === false){
             mouseoutTarget.style.backgroundImage = 'none';
         }
     }
     
+    function emptySpaces(board) {
+      return board.filter(emptySpace => emptySpace != "O" && emptySpace != "X")
+    }
+
+    function pvcWinner (board, player) {
+        if (
+         (board[0] == player.getAttribute('nameVal') && 
+          board[1] == player.getAttribute('nameVal') && 
+          board[2] == player.getAttribute('nameVal')) ||
+         (board[3] == player.getAttribute('nameVal') && 
+          board[4] == player.getAttribute('nameVal') && 
+          board[5] == player.getAttribute('nameVal')) ||
+         (board[6] == player.getAttribute('nameVal') && 
+          board[7] == player.getAttribute('nameVal') && 
+          board[8] == player.getAttribute('nameVal')) ||
+         (board[0] == player.getAttribute('nameVal') &&
+          board[3] == player.getAttribute('nameVal') && 
+          board[6] == player.getAttribute('nameVal')) ||
+         (board[1] == player.getAttribute('nameVal') && 
+          board[4] == player.getAttribute('nameVal') && 
+          board[7] == player.getAttribute('nameVal')) ||
+         (board[2] == player.getAttribute('nameVal') && 
+          board[5] == player.getAttribute('nameVal') && 
+          board[8] == player.getAttribute('nameVal')) ||
+         (board[0] == player.getAttribute('nameVal') && 
+          board[4] == player.getAttribute('nameVal') && 
+          board[8] == player.getAttribute('nameVal')) ||
+         (board[2] == player.getAttribute('nameVal') && 
+          board[4] == player.getAttribute('nameVal') && 
+          board[6] == player.getAttribute('nameVal'))
+         ) {
+            return true;
+         } else {
+            return false;
+         }
+    }
+
+    function compMinimax (nBoard, player) {
+        var openSpots = emptySpaces(nBoard);
+        if(pvcWinner(nBoard, playerOne.activePlayer)) {
+            return {score:-10}
+        } else if (pvcWinner(nBoard, computer.activePlayer)) {
+            return {score:10}
+        } else if (openSpots.length === 0) {
+            return {score:0}
+        }
+        var allMoves = [];
+        openSpots.forEach((openSpot, ind) => {
+            var move ={};
+            move.index = nBoard[openSpot];
+            nBoard[openSpot] = player.getAttribute('nameVal');
+            if (player === computer.activePlayer) {
+                var result = compMinimax(nBoard, playerOne.activePlayer);
+                move.score = result.score;
+            } else {
+                var result = compMinimax(nBoard, computer.activePlayer)
+                move.score = result.score;
+            }
+           
+         nBoard[openSpot] = move.index;
+         allMoves.push(move);
+        });
+        var bestMove;
+        if(player === computer.activePlayer) {
+            var bestScore = -10000;
+            allMoves.forEach((allMove, ind) => {
+                if(allMove.score > bestScore) {
+                    bestScore = allMove.score;
+                    bestMove = ind;
+                }
+            });
+        } else {
+            var bestScore = 10000;
+            allMoves.forEach((allMove, ind) => {
+                if(allMove.score < bestScore) {
+                    bestScore = allMove.score;
+                    bestMove = ind;
+                }
+            });
+        }
+        return allMoves[bestMove];
+    }
+    
+    
+
     function inputPlayerMove(clickTarget) {
-        if (clickTarget.classList.contains('alreadySelected') === false){
+        if (clickTarget.classList.contains('alreadySelected') === false && computer.activePlayer.classList.contains('comp') === false){
             
             if (playerOne.activePlayer.classList.contains('active')) {
                 clickTarget.classList.add('box-filled-1');
+                playerOne.activePlayer.classList.add('box-filled-1');
                 clickTarget.style.backgroundImage = 'url("img/o.svg")';
                 clickTarget.classList.add('alreadySelected');
                 clickTarget.style.cursor = 'default';
                 playerOne.playerMoves.push(clickTarget);
-                checkForWinner()
+                checkForWinner(playerOne.playerMoves);
                 return playerTwoActive();
             } 
 
@@ -118,21 +210,42 @@
                 clickTarget.classList.add('alreadySelected');
                 clickTarget.style.cursor = 'default';
                 playerTwo.playerMoves.push(clickTarget);
-                checkForWinner()
-                return playerOneActive()    
+                checkForWinner(playerTwo.playerMoves);
+                return playerOneActive();
             }
+        } else if (clickTarget.classList.contains('alreadySelected') === false && computer.activePlayer.classList.contains('comp')) {
+                
+                clickTarget.classList.add('box-filled-1');
+                playerOne.activePlayer.setAttribute('nameVal', 'O');
+                computer.activePlayer.setAttribute('nameVal', 'X');
+                clickTarget.style.backgroundImage = 'url("img/o.svg")';
+                clickTarget.classList.add('alreadySelected');
+                clickTarget.style.cursor = 'default';
+                playerOne.playerMoves.push(clickTarget);
+                newBoard.compBoard[clickTarget.getAttribute('cell-num')] = "O";
+                checkForWinner(playerOne.playerMoves);
+                return computerMove();
+            }
+    }
+
+    function computerMove() {
+        compMinimax(newBoard.compBoard, computer.activePlayer);
+        var compMove = compMinimax(newBoard.compBoard, computer.activePlayer).index
+        if (compMove != undefined) {
+            newBoard.allBoxesArray[compMove].classList.add('box-filled-2');
+            newBoard.allBoxesArray[compMove].style.backgroundImage = 'url("img/x.svg")';
+            newBoard.allBoxesArray[compMove].classList.add('alreadySelected');
+            newBoard.compBoard[compMove] = "X";
+            computer.playerMoves.push(newBoard.allBoxesArray[compMove]);
+            checkForWinner(computer.playerMoves);
         }
+        return playerOneActive();
     }
 
    //idea use filter to and charAt to filter out columns and rows     
-    function checkForWinner() {
-        if (playerOne.playerMoves.length > 2 || playerTwo.playerMoves.length > 2) {
-            var playerTurn;
-            if ( playerOne.activePlayer.classList.contains('active')) {
-                playerTurn = playerOne.playerMoves; 
-            } else {
-                playerTurn = playerTwo.playerMoves; 
-            }
+    function checkForWinner(playerTurn) {
+        if (playerOne.playerMoves.length > 2 || playerTwo.playerMoves.length > 2 || computer.playerMoves.length > 2) {
+            
             const moveCoords = playerTurn.map(moveCoord => moveCoord.getAttribute('data-row') + moveCoord.getAttribute('data-col'));
             const sortCoords = moveCoords.sort();
             console.log(sortCoords);
@@ -151,7 +264,7 @@
 
                 if (testCoord1.length === 3 || testCoord2.length === 3 || testCoord3.length === 3 || testCoord4.length === 3) {
                     finishGame.classList.remove('active');
-                    endGame();
+                    endGame(playerTurn);
                 } else if (sortCoords.length === 5  &&          finishGame.classList.contains('active')) {
                     tie();
                 }
@@ -161,17 +274,16 @@
     }
 
     //Game End Screen
-    function endGame() {
+    function endGame(winner) {
         boardGame.style.display = 'none';
         startGame.style.display = 'none';
         finishGame.style.display = 'block';
-        //finishGame.classList.remove('active');
-        if( playerOne.activePlayer.classList.contains('active')) {
+        if( winner === playerOne.playerMoves) {
             finishGame.classList.add('screen-win-one');
             message.textContent = playerOne.name.value + ' Wins';
         }
         
-        if( playerTwo.activePlayer.classList.contains('active')) {
+        if( winner === playerTwo.playerMoves || winner === computer.playerMoves) {
             finishGame.classList.add('screen-win-two');
             message.textContent = playerTwo.name.value + ' Wins';
         }
@@ -192,10 +304,12 @@
         finishGame.style.display = 'none';
         playerOne.playerMoves.length = 0;
         playerTwo.playerMoves.length = 0;
+        computer.playerMoves.length = 0;
         finishGame.classList.remove('screen-win-one', 'screen-win-two');
         finishGame.classList.add('active');
         message.textContent = '';
         playerOneActive();
+        newBoard.compBoard = [0, 1, 2, 3, 4, 5, 6, 7, 8,];
         newBoard.allBoxes.forEach((box2,j) => {
             box2.style.backgroundImage = 'none';
             box2.classList.remove('box-filled-1', 'box-filled-2');
@@ -234,6 +348,7 @@
         const liTwo = document.createElement('li');
         liTwo.textContent = 'Computer';
         playerTwo.activePlayer.appendChild(liTwo);
+        computer.activePlayer.classList.add('comp');
     });
         
         
@@ -254,38 +369,7 @@
         inputPlayerMove(clickTarget);
     });
 
-//}());
+}());
 
 
 
-//////////////////////////Archive for later use
-//forEach Loop
-//horizontalWin.forEach((b, index) => {
-//                if (playerOneMoves[0, 1, 2].coords = horizontalWin[0, 1, 2].coords) {
-//                    console.log('win');
-//                }    
-//            }); 
-
-
-//works for 1 horizontal win at a time
-//sortCoords.forEach((sortCoord, index) =>  {
-//                testCoord = sortCoord === (horizontalWin1[index]);
-//                //console.log(sortCoord);
-//                console.log(testCoord);
-//                return testCoord;
-//            });
-//            if (testCoord === true) {
-//                console.log('win');
-//            }        
-
-//Works for single horizontal win
-//            const testCoord3 = sortCoords.filter(sortCoord => sortCoord.charAt(0) === '2');
-
-//Original Startup Screen Page Load using IIFE
-//    var startScreen = (function() {
-//        boardGame.style.display = 'none';
-//        finishGame.style.display = 'none';
-//        startGame.style.display = 'block';
-//        finishGame.classList.add('active');
-//        playerOneActive();
-//    }());
